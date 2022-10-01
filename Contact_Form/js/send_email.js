2ebb5bf5510f8face26476f19bf8e5658bc30731
Alberto <albertiae.digital@gmail.com>
2022-09-30 20:12:56 -0400

    Modified the sendEmail function in the script

--- Contact_Form/js/send_email.js
+++ Contact_Form/js/send_email.js
@@ -9,7 +9,11 @@ function sendEmail(){
     From : document.getElementById("email").value,
     Subject : "Contact Form",
     Body : "You have a new request"
+    + "<br> Name: " + document.getElementById("name").value
+    + "<br> Email: " + document.getElementById("email").value
+    + "<br> Phone: " + document.getElementById("phone").value
+    + "<br> Message <br>: " + document.getElementById("name").value
   }).then(
-    message => alert(message)
+    message => alert("Message sent to" + getElementById("email"))
   );
 }

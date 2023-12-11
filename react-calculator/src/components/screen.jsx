1e454af9456240a59fb9ff8d942d5e455fb6c65c
jesusalberto18 <albertiae.digital@gmail.com>
2023-12-11 19:46:02 -0400

    Created screen.jsx

---
+++ react-calculator/src/components/screen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/screen.css'
+
+function Screen({ input }) {
+  return(
+    <div className='input'>
+      {input}
+    </div>
+  );
+}
+
+export default Screen;

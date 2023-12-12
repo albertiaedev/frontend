30b79d4f4d7711b260ffed4af685d650bf1cd231
jesusalberto18 <albertiae.digital@gmail.com>
2023-12-11 20:04:24 -0400

    Modified App.js

--- testimonials/src/App.js
+++ testimonials/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './components/testimonio.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <h1>This is our customers say about us:</h1>
+        <Testimonio
+          name='Shawn'
+          country='Philippines'
+          role='CEO'
+          workplace='ABC Inc.'
+          testimonial='Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+          image='Shawn'
+        />
+        <Testimonio
+          name='Sarah'
+          country='Nigeria'
+          role='HR Manager'
+          workplace='ABC Inc.'
+          testimonial='Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+          image='Sarah'
+        />
+        <Testimonio
+          name='Emma'
+          country='Canada'
+          role='Marketing Associate'
+          workplace='ABC Inc.'
+          testimonial='Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+          image='Emma'
+        />
+      </div>
     </div>
   );
 }

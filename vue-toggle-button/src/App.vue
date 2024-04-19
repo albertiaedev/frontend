a16e6ff1818d0dd46001563d8c3ed80391bb4aa9
Alberto Hernández <albertiae.digital@gmail.com>
2024-04-18 20:11:40 -0400

    ToggleButton

---
+++ vue-toggle-button/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <ToggleButton />
+</template>
+
+<script>
+import ToggleButton from './components/ToggleButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToggleButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  background: #d9d9d9;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-toggle-button/src/components/ToggleButton.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1 v-if="isVisible">
+            {{message}}
+        </h1>
+        <button v-on:click="toggleText()">
+            {{ isVisible? 'Hide Text' : 'Show Text' }}
+        </button>
+        <input v-model="message"> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueTest',
+    data() {
+        return {
+            message: 'Vue Test',
+            isVisible: true
+        }
+    },
+    methods: {
+        toggleText() {
+            this.isVisible = !this.isVisible;
+        },
+    }
+}
+</script>
+
+<style>
+input {
+    margin-left: 10px;
+    background: #d9d9d9;
+    border: solid #000 2px;
+    border-radius: 20px;
+    padding: 8px;
+}
+button {
+    background: #000;
+    color: #d9d9d9;
+    border: solid #000 2px;
+    border-radius: 20px;
+    font-size: medium;
+    padding: 8px;
+}
+</style>
